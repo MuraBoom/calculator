305fd04a9c6f737b0e10eb88168aa6978018747e
MuraBoom <95245285+MuraBoom@users.noreply.github.com>
2025-12-19 20:53:20 +0300

    запуск программы

--- calculator/main.py
+++ calculator/main.py
@@ -1,18 +1,10 @@
 import tkinter as tk
 from calculator_ui import CalculatorUI
 
+# запуск программы
 def main():
     root = tk.Tk()
     app = CalculatorUI(root)
-    
-    # Центрируем окно
-    root.update_idletasks()
-    width = root.winfo_width()
-    height = root.winfo_height()
-    x = (root.winfo_screenwidth() // 2) - (width // 2)
-    y = (root.winfo_screenheight() // 2) - (height // 2)
-    root.geometry(f'{width}x{height}+{x}+{y}')
-    
     root.mainloop()
 
 if __name__ == "__main__":
